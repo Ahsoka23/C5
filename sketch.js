a7cc75c6e1f06fdc26cd96dfccbc1d81d00c24d4
Ahsoka23 <89179461+Ahsoka23@users.noreply.github.com>
2022-04-20 14:14:25 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+//javascript
+var box  = ["hello",[1,2,3,4],true,4,5]; 
+
+console.log(box[1][2]); 
+
+// create an array for every planet in our solar system
+
+//just to print the planet that you are living now 
+
+
+//create an array of videogames 
+
+//print your top 3 
+
+function setup(){
+  createCanvas(400,400);
+
+ 
+
+  
+}
+
+function draw(){
+  background("white");
+ 
+
+}
